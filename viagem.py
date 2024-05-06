f3f36940fcb7aebcf6be263450fe42b303934a15
ronaldosnb <ronaldosnbatista@gmail.com>
2024-05-06 07:54:10 -0300

    pedir nome para o usuario

--- viagem.py
+++ viagem.py
@@ -1,5 +1,5 @@
 distancia = float(input("Digite a distância: "))
 velocidade = float(input("Digite a velocidade: "))
 tempo = distancia/velocidade
-
-print(f"você levara {tempo}hrs para chegar ao seu destino")
+motorista = input("Nome do motorista: ")
+print(f"{motorista} levara {tempo}hrs para chegar ao seu destino")
