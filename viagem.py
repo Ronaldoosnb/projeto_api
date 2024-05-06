771fafc523b5d3a97ee3a92492604a5470d5db2f
ronaldosnb <ronaldosnbatista@gmail.com>
2024-05-06 07:47:34 -0300

    v0.1

---
+++ viagem.py
@@ -0,0 +1,5 @@
+distancia = float(input("Digite a distância: "))
+velocidade = float(input("Digite a velocidade: "))
+tempo = distancia/velocidade
+
+print(f"você levara {tempo}hrs para chegar ao seu destino")
